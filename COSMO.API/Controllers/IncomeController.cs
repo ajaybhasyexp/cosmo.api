35482545ec5249fbc3a35b641b3844b0c9e42ca4
ajaybhasyexp <ajay.bhasy@experionglobal.com>
2019-05-23 00:02:51 +0530

    Added Controller

--- COSMO.API/Controllers/IncomeController.cs
+++ COSMO.API/Controllers/IncomeController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using COSMO.API.Resources;
+using COSMO.Business.Abstractions;
+using COSMO.Models.Exceptions;
+using COSMO.Models.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,48 @@ namespace COSMO.API.Controllers
     [ApiController]
     public class IncomeController : ControllerBase
     {
+        #region Private members
+
+        /// <summary>
+        /// The branch service for busines methods.
+        /// </summary>
+        private IIncomeService _incomeService { get; set; }
+
+        /// <summary>
+        /// The common resource file.
+        /// </summary>
+        private ICommonResource _commonResource { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Constructor for injection.
+        /// </summary>
+        /// <param name="branchServive">The branch service to inject.</param>
+        public IncomeController(IIncomeService incomeService, ICommonResource commonResource)
+        {
+            _incomeService = incomeService;
+            _commonResource = commonResource;
+        }
+
+        /// <summary>
+        /// The save/update method for BatchAssignment
+        /// </summary>
+        /// <param name="BatchAssignment"></param>
+        /// <returns>A saved or updated BatchAssignment entity.</returns>
+        [HttpPost]
+        public ResponseDto<Income> Save([FromBody]Income income)
+        {
+            ResponseDto<Income> response = new ResponseDto<Income>(_commonResource);
+            try
+            {
+                response.Data = _incomeService.Save(income);
+                return response;
+            }            
+            catch
+            {
+                return response.HandleException(response);
+            }
+        }
     }
 }
